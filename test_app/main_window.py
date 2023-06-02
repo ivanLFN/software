9fbcd62ae3b92d865e63879ce2ca10df09506e97
ivanLFN <LFN.current@yandex.com>
2023-06-02 07:04:38 -0700

    added start/stop for plots

--- test_app/main_window.py
+++ test_app/main_window.py
@@ -1,14 +1,11 @@
-from PyQt5.QtWidgets import QMainWindow, QTabWidget, QWidget, QHBoxLayout, QLabel, QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow
 from ui_wnd import Ui_MainWindow
 from widgets.header_widget import setup_header
-from widgets.tab_widget import setup_tab_widget
-from PyQt5.QtCore import Qt, QPoint, QRectF, QTimer
-from PyQt5.QtGui import QMouseEvent, QRegion, QPainterPath, QPixmap, QIcon
+from widgets.tab_widget import setup_tab_widget, StudyWidgetTab
+from PyQt5.QtCore import Qt, QPoint, QRectF
+from PyQt5.QtGui import QMouseEvent, QRegion, QPainterPath
 from connection import ConnectionAruino
 from widgets.dialogs_windows import ResponseConnectionDialog
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import numpy as np
 from database import DatabaseConnection
 
 
@@ -40,72 +37,12 @@ class MainWindow(QMainWindow):
         self.offset = QPoint()
 
         # База данных
-        db_connection = DatabaseConnection()
-        db_connection.insert_data("9.81,-0.32,9.45,-120,50,80")
-        db_connection.fetch_data()
-        db_connection.close_connection()
+        self.db_connection = DatabaseConnection()
+        self.db_connection.fetch_data()
+        self.db_connection.close_connection()
 
-
-
-        self.figure = Figure()
-        self.canvas = FigureCanvas(self.figure)
-        self.axes = self.figure.subplots(3, 1)
-
-        self.axes[0].set_ylabel('X')
-        self.axes[1].set_ylabel('Y')
-        self.axes[2].set_ylabel('Z')
-        self.axes[2].set_xlabel('Time')
-
-        self.canvas.draw()
-
-        self.data_x = []
-        self.data_y = []
-        self.data_z = []
-
-        self.timer = QTimer()
-        self.timer.setInterval(1000)  # Интервал обновления в миллисекундах
-        self.timer.timeout.connect(self.update_graph)
-        self.timer.start()
-
-        plot_layout = QVBoxLayout()
-        plot_layout.addWidget(self.canvas)
-        central_widget = QWidget(self)
-        central_layout = QVBoxLayout()
-        central_widget.setLayout(central_layout)
-        self.ui.widget.setLayout(plot_layout)
-
-
-
-    def update_graph(self):
-        # Генерация случайных данных для осей x, y, z (замените этот код на ваш код получения данных)
-        x = np.random.randint(0, 100)
-        y = np.random.randint(0, 100)
-        z = np.random.randint(0, 100)
-
-        # Добавление новых данных в списки
-        self.data_x.append(x)
-        self.data_y.append(y)
-        self.data_z.append(z)
-
-        self.data_x = self.data_x[-50:]
-        self.data_y = self.data_y[-50:]
-        self.data_z = self.data_z[-50:]
-
-        # Очистка графиков и отображение новых данных
-        self.axes[0].clear()
-        self.axes[1].clear()
-        self.axes[2].clear()
-        
-        self.axes[0].plot(self.data_x)
-        self.axes[1].plot(self.data_y)
-        self.axes[2].plot(self.data_z)
-
-        self.canvas.draw()
-
-
-
-
-
+        # Графики на 2 вкладке
+        self.dir_plots = StudyWidgetTab(self)
 
 
 
--- test_app/widgets/tab_widget.py
+++ test_app/widgets/tab_widget.py
@@ -1,6 +1,13 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTabWidget, QWidget, QVBoxLayout, QLabel
-from PyQt5.QtGui import QMouseEvent, QRegion, QPainterPath, QPixmap, QIcon
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
+from PyQt5.QtGui import QPixmap, QIcon
+from PyQt5.QtCore import QTimer, Qt
 import serial.tools.list_ports
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import numpy as np
+import threading
+import time
+
 
 def setup_tab_widget(main_window):
 
@@ -43,10 +50,112 @@ def setup_tab_widget(main_window):
         }
     """)
 
+    main_window.ui.checkBox_showplot.setStyleSheet('''
+         QCheckBox {
+        spacing: 5px;
+        font-size: 18px;
+        color: #333333;
+        }
+        
+        QCheckBox::indicator {
+            width: 30px;
+            height: 20px;
+        }
+        
+        QCheckBox::indicator:unchecked {
+            border: 2px solid #455a64;
+            background-color: white;
+            border-radius: 10px;
+        }
+        
+        QCheckBox::indicator:checked {
+            border: 2px solid #455a64;
+            background-color: #2d6577;
+            border-radius: 10px;
+        }
+    ''')    
+
+    
+
+
 def add_available_ports(main_window):
     ports = serial.tools.list_ports.comports()
     port_list = [port.device for port in ports]
 
     for port in port_list:
         main_window.ui.COM.addItem(port)
+
+
+class StudyWidgetTab():
+    def __init__(self, main_window):
+
+        main_window.ui.checkBox_showplot.stateChanged.connect(self.handle_checkBox_stateChanged)
+        
+        self.flag_stopped = True
+
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.axes = self.figure.subplots(3, 1)
+
+        self.axes[0].set_ylabel('X')
+        self.axes[1].set_ylabel('Y')
+        self.axes[2].set_ylabel('Z')
+        self.axes[2].set_xlabel('Time')
+
+        self.canvas.draw()
+
+        self.data_x = []
+        self.data_y = []
+        self.data_z = []
+
+        plot_layout = QVBoxLayout()
+        plot_layout.addWidget(self.canvas)
+        central_widget = QWidget()
+        central_layout = QVBoxLayout()
+        central_widget.setLayout(central_layout)
+        main_window.ui.widget.setLayout(plot_layout)
+
+
+    def run_thread(self):
+        while self.flag_stopped:
+            self.update_graph()  # Вызов метода
+            time.sleep(1)  # Приостановка выполнения на 1 секунду
+
+
+    def update_graph(self):
+        # Генерация случайных данных для осей x, y, z (замените этот код на ваш код получения данных)
+        x = np.random.randint(0, 100)
+        y = np.random.randint(0, 100)
+        z = np.random.randint(0, 100)
+
+        # Добавление новых данных в списки
+        self.data_x.append(x)
+        self.data_y.append(y)
+        self.data_z.append(z)
+
+        self.data_x = self.data_x[-50:]
+        self.data_y = self.data_y[-50:]
+        self.data_z = self.data_z[-50:]
+
+        # Очистка графиков и отображение новых данных
+        self.axes[0].clear()
+        self.axes[1].clear()
+        self.axes[2].clear()
+        
+        self.axes[0].plot(self.data_x)
+        self.axes[1].plot(self.data_y)
+        self.axes[2].plot(self.data_z)
+
+        self.canvas.draw()
+
+    
+    def handle_checkBox_stateChanged(self, state):
+        if state == Qt.Checked:
+            # self.start_timer()
+            self.flag_stopped = True
+            self.thread = threading.Thread(target=self.run_thread)
+            self.thread.start()
+        else:
+            # self.timer_turn_off()
+            self.flag_stopped = False
 
